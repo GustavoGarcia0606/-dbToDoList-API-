163d23af927de956d26ad1df9eb4b683604c3966
GustavoGarcia <gu.garciamarinho@gmail.com>
2025-07-31 22:01:07 -0300

    update

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors=require('cors');
-require('dotenv').config();
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 const tarefaRoutes = require('./routes/tarefaRoutes');
-app.use('/tarefas', tarefaRoutes);
+app.use('/', tarefaRoutes);
 
 app.listen(300, () => {
     console.log(`Servidor rodando na porta `);
